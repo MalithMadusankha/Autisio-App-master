a593a2a326a4def0105a81b11c2c06d29174bc13
Malith Madusankha <malithsony@gmail.com>
2023-10-31 01:39:45 +0530

    report

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,10 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ENDPOINT =
-  'https://fa78-2402-d000-8100-df84-9d70-b320-6f8a-2e49.ngrok-free.app/';
+  'https://e647-2402-d000-8100-40a2-b019-63e8-7cf8-4483.ngrok-free.app/';
 
 export const ANY_ENDPOINT =
-  'https://fa78-2402-d000-8100-df84-9d70-b320-6f8a-2e49.ngrok-free.app/';
+  'https://e647-2402-d000-8100-40a2-b019-63e8-7cf8-4483.ngrok-free.app/';
 
 export const ReadUser = async () => {
   try {
